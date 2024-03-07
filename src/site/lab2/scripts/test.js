0a6e37faa91622789a763e8d006995a8c3515eaa
resme <resmerdk@gmail.com>
2024-03-07 21:45:20 +0200

    lamp start

---
+++ src/site/lab2/scripts/test.js
@@ -0,0 +1,15 @@
+<div class="temp m1">…/div>
+  <script>
+    var sum = 0.1+0.2;
+    var result = document.getElementsByClassName("m1")[0];
+    result.innerHTML = "В Java Script " + "<br>"+ "  0.1 + 0.2 =  " + sum;
+    result.style.color = '#2d264a';
+</script>
+  <script>
+  document.write(9 + 10);
+</script>
+  window.alert(“Важлива інформація!”);
+  </script>
+  <script>
+  console.log(20 + 17);
+  </script>
